0c81a1049011f0a55b81ce0a0e9ab33c29e2705b
Sobhan Hota <shota@pentaho.com>
2025-02-25 10:40:13 -0500

    Added files

---
+++ env/spoon_10_2.bat
@@ -0,0 +1,4 @@
+set KETTLE_HOME=%CD%
+REM set PENTAHO_JAVA_HOME=C:\Program Files\Java\jdk-11
+set PENTAHO_JAVA_HOME=C:\Program Files\Java\jdk-17
+call C:\design-tools\10_2\spoon\data-integration\Spoon.bat
