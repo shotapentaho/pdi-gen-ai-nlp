065ea55188c14373bd167289f3329122342c5aca
shotapentaho <sobhan.hota@hitachivantara.com>
2025-03-04 14:04:52 -0500

    table names modified to make generic to adopt other LLMs

--- sql/open_ai_ddl.sql
+++ sql/open_ai_ddl.sql
@@ -1,15 +1,15 @@
-DROP TABLE IF EXISTS public.open_ai_bigrams;
-CREATE TABLE IF NOT EXISTS public.open_ai_bigrams
+DROP TABLE IF EXISTS public.nlp_bigrams;
+CREATE TABLE IF NOT EXISTS public.nlp_bigrams
 (
     chat_id double precision,
     bigrams_tagged text COLLATE pg_catalog."default",
 	created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 ) TABLESPACE pg_default;
-ALTER TABLE IF EXISTS public.open_ai_bigrams OWNER to postgres;
-ALTER TABLE IF EXISTS public.open_ai_bigrams OWNER to pdi_dev;
+ALTER TABLE IF EXISTS public.nlp_bigrams OWNER to postgres;
+ALTER TABLE IF EXISTS public.nlp_bigrams OWNER to pdi_dev;
 
-DROP TABLE IF EXISTS public.open_ai_chat_store;
-CREATE TABLE IF NOT EXISTS public.open_ai_chat_store
+DROP TABLE IF EXISTS public.nlp_chat_store;
+CREATE TABLE IF NOT EXISTS public.nlp_chat_store
 (
     chat_id double precision,
     question text COLLATE pg_catalog."default",
@@ -18,32 +18,32 @@ CREATE TABLE IF NOT EXISTS public.open_ai_chat_store
     created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
     genai_model character varying(255) COLLATE pg_catalog."default"
 )TABLESPACE pg_default;
-ALTER TABLE IF EXISTS public.open_ai_chat_store    OWNER to postgres;
-ALTER TABLE IF EXISTS public.open_ai_chat_store    OWNER to pdi_dev;
+ALTER TABLE IF EXISTS public.nlp_chat_store    OWNER to postgres;
+ALTER TABLE IF EXISTS public.nlp_chat_store    OWNER to pdi_dev;
 
-DROP TABLE IF EXISTS public.open_ai_chat_translated;
-CREATE TABLE IF NOT EXISTS public.open_ai_chat_translated
+DROP TABLE IF EXISTS public.nlp_chat_translated;
+CREATE TABLE IF NOT EXISTS public.nlp_chat_translated
 (
     natural_language text COLLATE pg_catalog."default",
     translated_to_english text COLLATE pg_catalog."default",
     chat_id double precision,
     created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 )TABLESPACE pg_default;
-ALTER TABLE IF EXISTS public.open_ai_chat_translated    OWNER to postgres;
-ALTER TABLE IF EXISTS public.open_ai_chat_translated    OWNER to pdi_dev;
+ALTER TABLE IF EXISTS public.nlp_chat_translated    OWNER to postgres;
+ALTER TABLE IF EXISTS public.nlp_chat_translated    OWNER to pdi_dev;
 	
-DROP TABLE IF EXISTS public.open_ai_coreferences;
-CREATE TABLE IF NOT EXISTS public.open_ai_coreferences
+DROP TABLE IF EXISTS public.nlp_coreferences;
+CREATE TABLE IF NOT EXISTS public.nlp_coreferences
 (
     chat_id double precision,
     coreferences_tagged text COLLATE pg_catalog."default",
     created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 ) TABLESPACE pg_default;
-ALTER TABLE IF EXISTS public.open_ai_coreferences OWNER to postgres;
-ALTER TABLE IF EXISTS public.open_ai_coreferences OWNER to pdi_dev;
+ALTER TABLE IF EXISTS public.nlp_coreferences OWNER to postgres;
+ALTER TABLE IF EXISTS public.nlp_coreferences OWNER to pdi_dev;
 
-DROP TABLE IF EXISTS public.open_ai_tagged_data;
-CREATE TABLE IF NOT EXISTS public.open_ai_tagged_data
+DROP TABLE IF EXISTS public.nlp_tagged_data;
+CREATE TABLE IF NOT EXISTS public.nlp_tagged_data
 (
     chat_id double precision,
     sentiment_tagged text COLLATE pg_catalog."default",
@@ -51,12 +51,12 @@ CREATE TABLE IF NOT EXISTS public.open_ai_tagged_data
     pos_tagged text COLLATE pg_catalog."default",
     created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 ) TABLESPACE pg_default;
-ALTER TABLE IF EXISTS public.open_ai_tagged_data OWNER to postgres;
-ALTER TABLE IF EXISTS public.open_ai_tagged_data OWNER to pdi_dev;
+ALTER TABLE IF EXISTS public.nlp_tagged_data OWNER to postgres;
+ALTER TABLE IF EXISTS public.nlp_tagged_data OWNER to pdi_dev;
 
 
-DROP TABLE IF EXISTS public.cosine_similarity_measure;
-CREATE TABLE IF NOT EXISTS public.cosine_similarity_measure
+DROP TABLE IF EXISTS public.nlp_cosine_similarity;
+CREATE TABLE IF NOT EXISTS public.nlp_cosine_similarity
 (
     chat_op1 text COLLATE pg_catalog."default",
     chat_op2 text COLLATE pg_catalog."default",
@@ -65,5 +65,15 @@ CREATE TABLE IF NOT EXISTS public.cosine_similarity_measure
     chat_id_sent_2 double precision,
     created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 )TABLESPACE pg_default;
-ALTER TABLE IF EXISTS public.cosine_similarity_measure    OWNER to postgres;
-ALTER TABLE IF EXISTS public.cosine_similarity_measure    OWNER to pdi_dev;
+ALTER TABLE IF EXISTS public.nlp_cosine_similarity    OWNER to postgres;
+ALTER TABLE IF EXISTS public.nlp_cosine_similarity    OWNER to pdi_dev;
+
+DROP TABLE IF EXISTS public.nlp_chat_language;
+CREATE TABLE IF NOT EXISTS public.nlp_chat_language
+(
+    natural_language text COLLATE pg_catalog."default",
+    chat_id double precision,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+) TABLESPACE pg_default;
+ALTER TABLE IF EXISTS public.nlp_chat_language     OWNER to pdi_dev;
+ALTER TABLE IF EXISTS public.nlp_chat_language     OWNER to postgres;
